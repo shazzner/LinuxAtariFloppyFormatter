06cd1669c76a686c57a4c5e6bc2b41ef84dcfd9d
Chris Hardee <shazzner@gmail.com>
2016-01-28 15:02:40 -0600

    Moved Gui code to separate file, added shell file

--- src/LaffGui.py
+++ src/LaffGui.py
@@ -7,9 +7,9 @@ class LaffGui:
         frame = Frame(master)
         frame.pack()
 
-        self.button = Button(
-            frame, text="QUIT", fg="red", command=frame.quit
-            )
+        self.button = Button(frame, text="Load ST Disk Image", command=self.say_hi)
+            #frame, text="Load ST Disk Image", fg="red", command=frame.quit
+
         self.button.pack(side=LEFT)
 
         self.hi_there = Button(frame, text="Hello", command=self.say_hi)
---
+++ src/LaffShell.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+# Shell Commands used
